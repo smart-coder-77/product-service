4064d42585b6d057942bc1e3a2cf792995afcbd2
Subham-coder-ui <choudhurysubham364@gmail.com>
2022-05-31 16:56:51 +0530

    added logs

--- src/main/java/com/smart/product/controller/ProductController.java
+++ src/main/java/com/smart/product/controller/ProductController.java
@@ -2,9 +2,9 @@ package com.smart.product.controller;
 
 import com.smart.product.entity.Product;
 import com.smart.product.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 /**
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/product")
+@Slf4j
 public class ProductController {
 
     @Autowired
@@ -20,26 +21,32 @@ public class ProductController {
 
     @PostMapping("/save")
     public String saveProduct(@RequestBody Product product){
+        log.info("Inside save product");
         return productService.saveProduct(product)?"Product saved successfully":"Product not saved";
     }
 
     @GetMapping(value = "/all")
+
     public List<Product> getListOfProducts(){
+        log.debug("get all products");
         return productService.getAllProduct();
     }
 
     @GetMapping("/")
     public Product getProductsByID(@RequestParam Long id){
+        log.debug("get products by ID");
         return productService.getProductById(id);
     }
 
     @GetMapping("/name/")
     public Product getProductByName(@RequestParam String name){
+        log.debug("get products by Name");
         return productService.getProductByName(name);
     }
 
     @DeleteMapping("/")
-    public String getProductByName(@RequestParam Long id){
+    public String deleteProductById(@RequestParam Long id){
+        log.debug("delete products by Id");
         return productService.deleteProductById(id)?"Product removed successfully":"Product not removed";
     }
 }
